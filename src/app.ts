4e5c0399cf5505e2fef337814d3134a80f17505c
IdanDover <111203178+IdanDover@users.noreply.github.com>
2024-03-07 13:50:28 +0200

    feat: now receives code when someone sends it

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import http from 'http';
-import express from 'express';
-import cors from 'cors';
-import { Server } from 'socket.io';
-import AppError from './errors/appError';
-import globalErrorHandler from './errors/errorController';
-import CodeBlockRouter from './routes/CodeBlockRouter';
-import { allowCors } from './utils/middlewareUtils';
+import http from "http";
+import express from "express";
+import cors from "cors";
+import { Server } from "socket.io";
+import AppError from "./errors/appError";
+import globalErrorHandler from "./errors/errorController";
+import CodeBlockRouter from "./routes/CodeBlockRouter";
+import { allowCors } from "./utils/middlewareUtils";
 
 const frontendUrl: string = process.env.FRONTEND_URL;
 
@@ -21,32 +21,30 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: frontendUrl,
-    methods: ['GET', 'POST', 'PUT'],
+    methods: ["GET", "POST", "PUT"],
   },
 });
 
-io.on('connection', (socket) => {
-  console.log('socket connected', socket.id);
+io.on("connection", (socket) => {
+  console.log("socket connected", socket.id);
 
-  socket.on('joinRoom', (roomName) => {
+  socket.on("joinRoom", (roomName) => {
     socket.join(roomName);
-    console.log(socket.rooms);
-
     console.log(`socket ${socket.id} joined room: ${roomName}`);
   });
 
-  socket.on('changedCode', (data: { room: string; code: string }) => {
-    socket.to(data.room).emit(data.code);
+  socket.on("sendCode", (data: { room: string; code: string }) => {
+    socket.to(data.room).emit("receiveCode", data.code);
   });
 
-  socket.on('disconnect', () => {
-    console.log('socket disconnect', socket.id);
+  socket.on("disconnect", () => {
+    console.log("socket disconnect", socket.id);
   });
 });
 
-app.use('/api/codeblock', CodeBlockRouter);
+app.use("/api/codeblock", CodeBlockRouter);
 
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
@@ -56,15 +54,15 @@ server.listen(port, () => {
   console.log(`Server is running on port:${port}`);
 });
 
-process.on('uncaughtException', (err) => {
+process.on("uncaughtException", (err) => {
   console.log(`${err.name} : ${err.message}`);
-  console.log('UNCAUGHT EXCEPTION 💥 Shutting down...');
+  console.log("UNCAUGHT EXCEPTION 💥 Shutting down...");
   process.exit(1);
 });
 
-process.on('unhandledRejection', (err: Error) => {
+process.on("unhandledRejection", (err: Error) => {
   console.log(`Error name-${err.name} : Error message-${err.message}`);
-  console.log('UNHANDLER REJECTION 💥 Shutting down...');
+  console.log("UNHANDLER REJECTION 💥 Shutting down...");
   server.close(() => {
     process.exit(1);
   });
