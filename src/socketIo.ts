20aa8bca48fefeacd03eb1da957e0e49d232eb11
Idan Dover <idan.dava@gmail.com>
2024-03-09 19:26:38 +0200

    changed logic for sending code

--- src/socketIo.ts
+++ src/socketIo.ts
@@ -2,6 +2,8 @@ import { Server } from 'socket.io';
 
 export default function initSocketServer(server: any, io: Server) {
   io.on('connection', (socket) => {
+    console.log('socket connected', socket.id);
+
     socket.on('joinRoom', async (roomName) => {
       if (socket.rooms.has(roomName)) {
         return;
@@ -17,22 +19,35 @@ export default function initSocketServer(server: any, io: Server) {
         socketsLength === 0 ? 'instructor' : 'student' // the first to join is an instructor and the second is a student
       );
       socket.join(roomName);
+      console.log(`socket ${socket.id} joined room: ${roomName}`);
     });
 
-    socket.on('sendCode', (data: { room: string; code: string }) => {
-      if (!socket.rooms.has(data.room)) {
-        return;
+    socket.on(
+      'sendCode',
+      (data: { room: string; role: string; code: string }) => {
+        if (!socket.rooms.has(data.room)) {
+          return;
+        }
+
+        if (data.role === 'instructor') {
+          return;
+        }
+
+        socket.to(data.room).emit('receiveCode', data.code);
       }
-
-      socket.to(data.room).emit('receiveCode', data.code);
-    });
+    );
 
     socket.on('leaveRoom', (roomName) => {
       if (!socket.rooms.has(roomName)) {
         return;
       }
+      console.log(`socket ${socket.id} left room: ${roomName}`);
 
       socket.leave(roomName);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('socket disconnect', socket.id);
     });
   });
 }
